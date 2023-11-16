b9d7192ea7048c8e4c723d46adbf5bc47d84f141
Ilir Koliqi <yll.rukiqi@webintel.me>
2023-11-16 11:25:39 +0100

    Maybe final ??

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,43 +1,60 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Product } from '../shared/product.model';
-import { DataService } from '../shared/data.service';
-import { Subscription } from 'rxjs';
-import { ProductService } from '../shared/product.service';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Product} from '../shared/product.model';
+import {DataService} from '../shared/data.service';
+import {delay, map, Subscription} from 'rxjs';
+import {ProductService} from '../shared/product.service';
 
 @Component({
-  selector: 'products',
-  templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+    selector: 'products',
+    templateUrl: './products.component.html',
+    styleUrls: ['./products.component.css']
 })
-export class ProductsComponent implements OnInit,OnDestroy {
-  products:Product[] = [];
-  selectedProduct:Product | null;
-  private productsSubscription: Subscription;
-  constructor(private productService: ProductService,private dataService: DataService){}
+export class ProductsComponent implements OnInit, OnDestroy {
+    products: Product[] = [];
+    selectedProduct: Product | null;
+    private productsSubscription: Subscription;
 
-  ngOnInit() {
-    this.productsSubscription = this.productService.products$.subscribe(products => {
-      this.products = products;
-    });
-    this.dataService.fetchProducts();
-  }
-
-  onSelectedProduct(product:Product){
-    this.selectedProduct = product
-  }
-
-  onCloseProductDetails(){
-    this.selectedProduct = null
-  }
-
-  trackByProductId(index: number, product: Product) {
-    return product ? product.id : undefined;
-  }
-
-
-  ngOnDestroy() {
-    if (this.productsSubscription) {
-      this.productsSubscription.unsubscribe();
+    constructor(private productService: ProductService, private dataService: DataService) {
     }
-  }
+
+    ngOnInit() {
+        if (this.productService.getProductsLength() === 0) this.loadData();
+
+        this.productsSubscription = this.productService.products$.subscribe(products => {
+            this.products = products;
+        });
+
+    }
+
+    loadData() {
+        this.dataService.fetchProducts().subscribe(
+            (modifiedProducts) => {
+                this.productService.setProducts(modifiedProducts);
+                this.dataService.isFetching.next(false)
+            },
+            (error) => {
+                console.error('Product fetching error', error);
+
+            }
+        );
+    }
+
+    onSelectedProduct(product: Product) {
+        this.selectedProduct = product
+    }
+
+    onCloseProductDetails() {
+        this.selectedProduct = null
+    }
+
+    trackByProductId(index: number, product: Product) {
+        return product ? product.id : undefined;
+    }
+
+
+    ngOnDestroy() {
+        if (this.productsSubscription) {
+            this.productsSubscription.unsubscribe();
+        }
+    }
 }
--- src/app/shared/data.service.ts
+++ src/app/shared/data.service.ts
@@ -1,41 +1,27 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Product } from './product.model';
-import {delay, map, Subject} from 'rxjs';
-import { ProductService } from './product.service';
+import {HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {Product} from './product.model';
+import {delay, map,Subject} from 'rxjs';
+import {ProductService} from './product.service';
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class DataService {
-  private api:string = 'https://my-json-server.typicode.com/jubs16/Products/Products';
-  isFetching = new Subject<boolean>()
-  constructor(private http: HttpClient, private productService: ProductService) {}
+    private api: string = 'https://my-json-server.typicode.com/jubs16/Products/Products';
+    isFetching = new Subject<boolean>()
+
+    constructor(private http: HttpClient, private productService: ProductService) {}
 
     fetchProducts() {
-        if (this.productService.getProductsLength() === 0) {
-            this.isFetching.next(true);
-
-            this.http
-                .get<Product[]>('/assets/products.json')
-                .pipe(
-                    delay(1000),
-                    map((products) => products.map((product) => ({ ...product, id: this.generateRandomId(), quantity: 0 })))
-                )
-                .subscribe(
-                    (modifiedProducts) => {
-                        this.productService.setProducts(modifiedProducts);
-                        this.isFetching.next(false);
-                    },
-                    (error) => {
-                        console.error('Product fetching error', error);
-                        this.isFetching.next(false);
-                    }
-                );
-        }
+        this.isFetching.next(true)
+        return this.http.get<Product[]>('/assets/products.json').pipe(
+            delay(1000),
+            map((products) => products.map((product) => ({...product, id: this.generateRandomId(), quantity: 0})))
+        )
     }
 
-  generateRandomId() {
-    return Math.floor(Math.random() * 1000000) + 1;
-  }
+    generateRandomId() {
+        return Math.floor(Math.random() * 1000000) + 1;
+    }
 }
